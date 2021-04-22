18ded77e90db5baa355334f06776500eae4ae11f
RohitPr <bladekiller95@gmail.com>
2021-04-22 10:55:18 +0530

    Commit

--- theatre.py
+++ theatre.py
@@ -10,7 +10,7 @@ square = int(number[2])
 
 
 if length % square == 0 and width % square == 0:
-    count = int(length/square * width/square)
+    count = int(length/square + width/square)
 else:
     count = math.ceil(length/square) + math.ceil(width/square)
 print(count)
