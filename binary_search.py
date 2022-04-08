b2673317596e47044e2aa9c17a858d00abc51bb0
RohitPr <bladekiller95@gmail.com>
2022-04-08 11:02:52 +0530

    update

---
+++ binary_search.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        low, high = 0, len(nums)-1
+
+        while low <= high:
+            mid = (low+high)//2
+
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                low = mid+1
+            else:
+                high = mid-1
+
+        return -1
---
+++ max_subarray.py
@@ -0,0 +1,26 @@
+# Given an integer array nums, find the contiguous subarray(containing at least one number) which has the largest sum and return its sum.
+
+# A subarray is a contiguous part of an array.
+
+
+# Example 1:
+
+# Input: nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+# Output: 6
+# Explanation: [4, -1, 2, 1] has the largest sum = 6.
+
+# Example 2:
+
+# Input: nums = [1]
+# Output: 1
+
+class Solution:
+    def maxSubArray(self, nums: list[int]) -> int:
+        cur = 0
+        theMax = nums[0]
+
+        for i in nums:
+            cur = max(i, i + cur)
+            theMax = max(cur, theMax)
+
+        return theMax
