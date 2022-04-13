5ae3839b429b957e7a12ce33b3696bd4f9d23ce4
RohitPr <bladekiller95@gmail.com>
2022-04-13 22:23:45 +0530

    Update

---
+++ plus_one.py
@@ -0,0 +1,41 @@
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+# Increment the large integer by one and return the resulting array of digits.
+
+
+# Example 1:
+
+# Input: digits = [1, 2, 3]
+# Output: [1, 2, 4]
+# Explanation: The array represents the integer 123.
+# Incrementing by one gives 123 + 1 = 124.
+# Thus, the result should be[1, 2, 4].
+
+# Example 2:
+
+# Input: digits = [9]
+# Output: [1, 0]
+# Explanation: The array represents the integer 9.
+# Incrementing by one gives 9 + 1 = 10.
+# Thus, the result should be[1, 0].
+
+
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        digits = digits[::-1]
+        one, i = 1, 0
+
+        while one:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+
+                i += 1
+            else:
+                digits.append(1)
+                one = 0
+
+        return digits[::-1]
