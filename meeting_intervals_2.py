18750f2087968f43804265ac99b43160440c7b90
RohitPr <bladekiller95@gmail.com>
2022-04-14 22:22:50 +0530

    Update

---
+++ meeting_intervals_2.py
@@ -0,0 +1,36 @@
+# Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.)
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+# Different Than Meeting Room 1 as if a meeting room is empty, we can use it for other meetings
+
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+
+    def min_meeting_rooms(self, intervals: list[Interval]) -> int:
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+
+        res, count = 0, 0
+        s, e = 0, 0
+
+        while s < len(intervals):
+            if start[s] < end[e]:
+                s += 1
+                count += 1
+            else:
+                e += 1
+                count -= 1
+            res = max(res, count)
+
+        return res
