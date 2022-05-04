33d21544fee335a0a214fbd247dc37a39bf44101
RohitPr <bladekiller95@gmail.com>
2022-05-04 21:08:45 +0530

    Update

--- backspace_str_compare.py
+++ backspace_str_compare.py
@@ -25,3 +25,27 @@ class Solution:
 
     def backspaceCompare(self, s: str, t: str) -> bool:
         return (self.strCheck(s) == self.strCheck(t))
+
+
+# O(n) Runtime, O(1) Space
+
+class Solution:
+
+    def strCheck(self, s):
+        t = ''
+        l = 0
+
+        for a in range(len(s)):
+            if s[a] != "#":
+                t += s[a]
+                l += 1
+
+            if s[a] == "#":
+                if t:
+                    t = t[0:l-1]
+                    l -= 1
+
+        return t
+
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        return (self.strCheck(s) == self.strCheck(t))
