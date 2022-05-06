9a39ad3d69b6b780d0f90ec4e40c59e726987a3d
RohitPr <bladekiller95@gmail.com>
2022-05-06 21:26:33 +0530

    Update

--- remove_adjacent_string.py
+++ remove_adjacent_string.py
@@ -1,28 +1,19 @@
-# You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
+# You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
 
-# We repeatedly make k duplicate removals on s until we no longer can.
+# We repeatedly make duplicate removals on s until we no longer can.
 
-# Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+# Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
 
 
 # Example 1:
 
-# Input: s = "abcd", k = 2
-# Output: "abcd"
-# Explanation: There's nothing to delete.
-# Example 2:
-
-# Input: s = "deeedbbcccbdaa", k = 3
-# Output: "aa"
+# Input: s = "abbaca"
+# Output: "ca"
 # Explanation:
-# First delete "eee" and "ccc", get "ddbbbdaa"
-# Then delete "bbb", get "dddaa"
-# Finally delete "ddd", get "aa"
-
-# https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
+# For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
 
 class Solution:
-    def removeDuplicates(self, s: str, k: int) -> str:
+    def removeDuplicates(self, s: str) -> str:
         stack = []
         res = ""
 
@@ -32,10 +23,10 @@ class Solution:
             else:
                 stack.append([a, 1])
 
-            if stack[-1][1] == k:
+            if stack[-1][1] == 2:
                 stack.pop()
 
-        for char, count in stack:
-            res += char * count
+        for char, val in stack:
+            res += char * val
 
         return res
