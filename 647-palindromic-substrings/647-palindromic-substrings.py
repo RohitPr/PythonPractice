9f72481baa4f44e07892ed2aa71d50a04a3f3490
Rohit Prasad <30986339+RohitPr@users.noreply.github.com>
2022-05-22 12:30:47 +0530

    Time: 171 ms (58.63%), Space: 13.9 MB (75.82%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -2,19 +2,23 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         res = 0
         
+        def countP(l, r):
+            count = 0
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+            
+            return count
+        
         for a in range(len(s)):
             # Odd Lenght Substrings
-            l = r = a
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                res += 1
-                l -= 1
-                r += 1
-            
+            res += countP(a,a)
             # Even Length Substring
-            l, r = a, a+1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                res += 1
-                l -= 1
-                r += 1
+            res += countP(a, a+1)
+        
+        return res    
             
-        return res
+       
+        
+        
