0744aafc9a8bb330666b83b0b1dcfecbdb1c282c
Rohit Prasad <30986339+RohitPr@users.noreply.github.com>
2022-05-28 00:36:27 +0530

    Time: 174 ms (7.06%), Space: 15.5 MB (50.52%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,26 +1,25 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        l, r = 0, len(nums) - 1
         
-        def binSearch(nums, target, flag):
-            res, l, r = -1, 0, len(nums) - 1
+        tarL, tarR = -1, -1
+        
+        while l <= r:
+            mid = (l + r) // 2
             
-            while l <= r:
-                mid = (l + r) // 2
-
-                if nums[mid] == target:
-                    res = mid
-                    if flag:
-                        l = mid + 1
-                    else:
-                        r = mid - 1
-                        
-                elif nums[mid] > target:
-                    r = mid - 1
-
-                else:
-                    l = mid + 1
-            return res
-
-        left, right = binSearch(nums, target, False), binSearch(nums, target, True) 
+            if nums[mid] == target:
+                tarL, tarR = mid, mid
+                while (mid - 1)  >= 0 and nums[mid- 1] == target:
+                    mid -= 1
+                    tarL = mid
+                while (mid + 1) < len(nums) and nums[mid + 1] == target:
+                    mid += 1
+                    tarR = mid 
+                break   
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
         
-        return [left, right] 
+        return([tarL, tarR])
+        
