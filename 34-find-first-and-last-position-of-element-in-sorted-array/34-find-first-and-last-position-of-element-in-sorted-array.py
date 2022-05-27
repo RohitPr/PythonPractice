f292d096ae9dd9be9e4ae34d246950a5a14ed964
Rohit Prasad <30986339+RohitPr@users.noreply.github.com>
2022-05-28 00:38:58 +0530

    Time: 143 ms (23.10%), Space: 15.5 MB (50.52%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,25 +1,51 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l, r = 0, len(nums) - 1
         
-        tarL, tarR = -1, -1
-        
-        while l <= r:
-            mid = (l + r) // 2
+        def binSearch(nums, target, flag):
+            res, l, r = -1, 0, len(nums) - 1
             
-            if nums[mid] == target:
-                tarL, tarR = mid, mid
-                while (mid - 1)  >= 0 and nums[mid- 1] == target:
-                    mid -= 1
-                    tarL = mid
-                while (mid + 1) < len(nums) and nums[mid + 1] == target:
-                    mid += 1
-                    tarR = mid 
-                break   
-            elif nums[mid] > target:
-                r = mid - 1
-            else:
-                l = mid + 1
+            while l <= r:
+                mid = (l + r) // 2
+
+                if nums[mid] == target:
+                    res = mid
+                    if flag:
+                        l = mid + 1
+                    else:
+                        r = mid - 1
+                        
+                elif nums[mid] > target:
+                    r = mid - 1
+
+                else:
+                    l = mid + 1
+            return res
+
+        left, right = binSearch(nums, target, False), binSearch(nums, target, True) 
         
-        return([tarL, tarR])
+        return [left, right] 
+    
+# class Solution:
+#     def searchRange(self, nums: List[int], target: int) -> List[int]:
+#         l, r = 0, len(nums) - 1
         
+#         tarL, tarR = -1, -1
+        
+#         while l <= r:
+#             mid = (l + r) // 2
+            
+#             if nums[mid] == target:
+#                 tarL, tarR = mid, mid
+#                 while (mid - 1)  >= 0 and nums[mid- 1] == target:
+#                     mid -= 1
+#                     tarL = mid
+#                 while (mid + 1) < len(nums) and nums[mid + 1] == target:
+#                     mid += 1
+#                     tarR = mid 
+#                 break   
+#             elif nums[mid] > target:
+#                 r = mid - 1
+#             else:
+#                 l = mid + 1
+        
+#         return([tarL, tarR])
