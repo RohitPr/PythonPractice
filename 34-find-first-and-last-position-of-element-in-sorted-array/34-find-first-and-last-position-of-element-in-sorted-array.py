1a99d29f4627c6151e0fd32931f3ad7ea2e6b3c8
RohitPr <bladekiller95@gmail.com>
2022-05-28 10:26:23 +0530

    Update

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,9 +1,9 @@
 # class Solution:
-#     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
+#     def searchRange(self, nums: list[int], target: int) -> list[int]:
+
 #         def binSearch(nums, target, flag):
 #             res, l, r = -1, 0, len(nums) - 1
-            
+
 #             while l <= r:
 #                 mid = (l + r) // 2
 
@@ -13,7 +13,7 @@
 #                         l = mid + 1
 #                     else:
 #                         r = mid - 1
-                        
+
 #                 elif nums[mid] > target:
 #                     r = mid - 1
 
@@ -21,31 +21,31 @@
 #                     l = mid + 1
 #             return res
 
-#         left, right = binSearch(nums, target, False), binSearch(nums, target, True) 
-        
-#         return [left, right] 
-    
+#         left, right = binSearch(nums, target, False), binSearch(nums, target, True)
+
+#         return [left, right]
+
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
+    def searchRange(self, nums: list[int], target: int) -> list[int]:
         l, r = 0, len(nums) - 1
-        
+
         tarL, tarR = -1, -1
-        
+
         while l <= r:
             mid = (l + r) // 2
-            
+
             if nums[mid] == target:
                 tarL, tarR = mid, mid
-                while (mid - 1)  >= 0 and nums[mid- 1] == target:
+                while (mid - 1) >= 0 and nums[mid - 1] == target:
                     mid -= 1
                     tarL = mid
                 while (mid + 1) < len(nums) and nums[mid + 1] == target:
                     mid += 1
-                    tarR = mid 
-                break   
+                    tarR = mid
+                break
             elif nums[mid] > target:
                 r = mid - 1
             else:
                 l = mid + 1
-        
+
         return([tarL, tarR])
